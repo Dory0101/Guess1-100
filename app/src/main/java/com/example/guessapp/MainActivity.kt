9614d1b289f3e2cba6a0cca0d469b2828270e8e1
Doreen <doreen7932@gmail.com>
2022-03-04 10:30:29 +0800

    MainActivity guess a number

--- app/src/main/java/com/example/guessapp/MainActivity.kt
+++ app/src/main/java/com/example/guessapp/MainActivity.kt
@@ -2,14 +2,26 @@ package com.example.guessapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.guessapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //get view
+    }
+
+
+    fun guess(view : View) {
+        var secret = (2..10).random()
+        var num = binding.edNumber.text.toString().toInt()
+        if (num != secret) {
+            println("Wrong! The secret number is $secret!")
+        } else {
+            println("Bingo! you got the number$secret")
+        }
     }
 }
